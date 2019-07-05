dc65590f1e5abb06a475c3958989a66a67760954
Erlend Åmdal <erlend@aamdal.com>
2019-07-05 17:28:44 +0200

    What is forge?

--- src/main/java/com/jamieswhiteshirt/clotheslinefabric/common/Util.java
+++ src/main/java/com/jamieswhiteshirt/clotheslinefabric/common/Util.java
@@ -6,14 +6,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import javax.annotation.Nullable;
 
 public class Util {
-    /**
-     * Forge really likes annotation magic. This makes static analysis tools shut up.
-     */
-    @SuppressWarnings("ConstantConditions")
-    public static <T> T nonNullInjected() {
-        return null;
-    }
-
     public static boolean isCreativePlayer(@Nullable Entity entity) {
         return entity instanceof PlayerEntity && ((PlayerEntity) entity).isCreative();
     }
