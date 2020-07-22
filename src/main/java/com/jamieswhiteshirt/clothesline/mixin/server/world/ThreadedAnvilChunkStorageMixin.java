64a621ead8ff2836976c03e3bb38814d3c70f0ca
Erlend Åmdal <erlend@aamdal.com>
2020-07-23 00:40:10 +0200

    Improve ThreadedAnvilChunkStorageMixin

--- src/main/java/com/jamieswhiteshirt/clothesline/mixin/server/world/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/com/jamieswhiteshirt/clothesline/mixin/server/world/ThreadedAnvilChunkStorageMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.chunk.WorldChunk;
 import net.minecraft.world.storage.VersionedChunkStorage;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,16 +28,21 @@ public abstract class ThreadedAnvilChunkStorageMixin extends VersionedChunkStora
 
     @Inject(
         at = @At("RETURN"),
+        method = "sendChunkDataPackets(Lnet/minecraft/server/network/ServerPlayerEntity;[Lnet/minecraft/network/Packet;Lnet/minecraft/world/chunk/WorldChunk;)V"
+    )
+    private void sendChunkDataPackets(ServerPlayerEntity player, Packet<?>[] packets, WorldChunk chunk, CallbackInfo ci) {
+        ChunkWatchCallback.WATCH.invoker().accept(player.world, chunk.getPos(), player);
+    }
+
+    @Inject(
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/network/ServerPlayerEntity;sendUnloadChunkPacket(Lnet/minecraft/util/math/ChunkPos;)V",
+            shift = At.Shift.AFTER
+        ),
         method = "sendWatchPackets(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/util/math/ChunkPos;[Lnet/minecraft/network/Packet;ZZ)V"
     )
     private void sendWatchPackets(ServerPlayerEntity player, ChunkPos pos, Packet<?>[] packets, boolean previouslyWatching, boolean currentlyWatching, CallbackInfo ci) {
-        if (player.world == world) {
-            if (currentlyWatching && !previouslyWatching) {
-                ChunkWatchCallback.WATCH.invoker().accept(world, pos, player);
-            }
-            if (!currentlyWatching && previouslyWatching) {
-                ChunkWatchCallback.UNWATCH.invoker().accept(world, pos, player);
-            }
-        }
+        ChunkWatchCallback.UNWATCH.invoker().accept(world, pos, player);
     }
 }
